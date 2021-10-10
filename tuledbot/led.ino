ac273115ddc13ed32648f17eadb8e53a82fb3d64
lucassargeir <sargeirolucas@gmail.com>
2021-10-09 23:41:39 -0300

    Add funções do LED

--- tuledbot/led.ino
+++ tuledbot/led.ino
@@ -1,6 +1,30 @@
 #include <Arduino.h>
 
-// ledInit (declarar o modo dos pinos)
-// ledTurnOn
-// ledTurnOff
-// ledBlink
+int PIN_LED;
+
+void ledInit(int PIN){
+    PIN_LED = PIN;
+  
+    pinMode(PIN_LED, OUTPUT);
+    digitalWrite(PIN_LED, LOW);
+}
+
+void ledTurnOn(){
+  Serial.print("--- Led Aceso ---");
+  digitalWrite(PIN_LED, HIGH);
+}
+
+
+void ledTurnOff(){
+  Serial.print("--- Led Apagado ---");
+  digitalWrite(PIN_LED, LOW);
+}
+
+
+void ledBlink(int time){
+    Serial.print("--- Piscando Led ---");
+    
+    ledTurnOn();
+    delay(time);
+    ledTurnOff();
+}
--- tuledbot/tuledbot.ino
+++ tuledbot/tuledbot.ino
@@ -6,16 +6,21 @@ void otaInit();
 
 void wifiLoop();
 void otaLoop();
+void ledBlink(int time);
 
 void setup() {
     serialInit();
     wifiInit(false);
     otaInit();
+    ledInit(D1);
 }
 
 void loop() {
     wifiLoop();
     otaLoop();
+    
+    ledBlink(10);
+    delay(1000);
 }
 
 void serialInit(){
